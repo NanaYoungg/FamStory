af4327bbdb1077d414f8480e413280db9e224d72
nnsyu <nnsyu@naver.com>
2021-01-08 23:11:58 +0900

    1. 감정표현 기능 구현    - Main, Sub 감정 리스트
    2. 프로필 기능 구현
       - 가족사진 등록
       - 가훈 등록
       - 프로필 보기 및 수정
       - 호칭 변경
    
    3. SharedManager 추가
    
    4. RecyclerAdapter 추가
       - 하나의 어댑터로 간단한 뷰홀더 연동 가능
       - 추가로 데이터를 받아와야하는건 따로 Adapter 생성해야함
    
    5. ViewHolderStruct 추가
       - 뷰홀더 클래스 관리

---
+++ app/src/main/java/com/hongsam/famstrory/activitie/MainActivity.java
@@ -0,0 +1,130 @@
+package com.hongsam.famstrory.activitie;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import com.hongsam.famstrory.define.Define;
+import com.hongsam.famstrory.R;
+import com.hongsam.famstrory.fragment.CalendarFragment;
+import com.hongsam.famstrory.fragment.EmotionFragment;
+import com.hongsam.famstrory.fragment.FamCreateFragment;
+import com.hongsam.famstrory.fragment.LetterListFragment;
+import com.hongsam.famstrory.fragment.LetterWriteFragment;
+import com.hongsam.famstrory.fragment.MenuFragment;
+import com.hongsam.famstrory.fragment.ProfileFragment;
+import com.hongsam.famstrory.fragment.SettingFragment;
+import com.hongsam.famstrory.fragment.TimeLineFragment;
+import com.hongsam.famstrory.util.SharedManager;
+
+public class MainActivity extends AppCompatActivity{
+    final String TAG = "MainActivity";
+
+    InputMethodManager imm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        SharedManager.getInstance(this);
+
+        changeFragment(Define.FRAGMENT_ID_EMOTION);
+    }
+
+
+
+    Fragment fragment = null;
+
+    public void changeFragment(int fragmentId) {
+
+        switch (fragmentId) {
+            case Define.FRAGMENT_ID_MENU:
+                fragment = new MenuFragment();
+                break;
+
+            case Define.FRAGMENT_ID_FAM_CREATE:
+                fragment = new FamCreateFragment();
+                break;
+
+            case Define.FRAGMENT_ID_TIME_LINE:
+                fragment = new TimeLineFragment();
+                break;
+
+            case Define.FRAGMENT_ID_CALENDAR:
+                fragment = new CalendarFragment();
+                break;
+
+            case Define.FRAGMENT_ID_LETTER_LIST:
+                fragment = new LetterListFragment();
+                break;
+
+            case Define.FRAGMENT_ID_LETTER_WRITE:
+                fragment = new LetterWriteFragment();
+                break;
+
+            case Define.FRAGMENT_ID_EMOTION:
+                fragment = new EmotionFragment();
+                break;
+
+            case Define.FRAGMENT_ID_PROFILE:
+                fragment = new ProfileFragment();
+                break;
+
+            case Define.FRAGMENT_ID_SETTING:
+                fragment = new SettingFragment();
+                break;
+
+            default:
+                break;
+        }
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+                for (int i = 0; i < getSupportFragmentManager().getBackStackEntryCount(); ++i) {
+                    try {
+                        getSupportFragmentManager().popBackStack();
+                    } catch (IllegalStateException e) {
+                        if (getSupportFragmentManager() != null && !getSupportFragmentManager().isStateSaved()) {
+                            getSupportFragmentManager().popBackStack();
+                        }
+                    }
+                }
+
+                ft.replace(R.id.basic, fragment);
+
+                if (fragment.isStateSaved()) {
+                    ft.commitAllowingStateLoss();
+                } else {
+                    ft.commit();
+                }
+            }
+        });
+    }
+
+    public void showKeyboard(final EditText et, final boolean flag) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (flag) {
+                    et.requestFocus();
+                    imm.showSoftInput(et, 0);
+                } else {
+                    imm.hideSoftInputFromWindow(et.getWindowToken(), 0);
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/hongsam/famstrory/define/Define.java
@@ -0,0 +1,31 @@
+package com.hongsam.famstrory.define;
+
+public class Define {
+    public static final int FRAGMENT_ID = 1000;
+    public static final int FRAGMENT_ID_MENU = FRAGMENT_ID + 1;
+    public static final int FRAGMENT_ID_FAM_CREATE = FRAGMENT_ID + 2;
+    public static final int FRAGMENT_ID_TIME_LINE = FRAGMENT_ID + 3;
+    public static final int FRAGMENT_ID_CALENDAR = FRAGMENT_ID + 4;
+    public static final int FRAGMENT_ID_LETTER_LIST = FRAGMENT_ID + 5;
+    public static final int FRAGMENT_ID_LETTER_WRITE = FRAGMENT_ID + 6;
+    public static final int FRAGMENT_ID_EMOTION = FRAGMENT_ID + 7;
+    public static final int FRAGMENT_ID_PROFILE = FRAGMENT_ID + 8;
+    public static final int FRAGMENT_ID_SETTING = FRAGMENT_ID + 9;
+
+    public static final String KEY_FAMILY_NAME = "KEY_FAMILY_NAME";
+    public static final String KEY_TEST = "KEY_TEST";
+
+    public static final int VIEWTYPE_MEMBER = 0;
+
+//    public static int MEMBER_GRAND_MOTHER = 0;    // 할머니
+//    public static int MEMBER_GRAND_FATHER = 1;    // 할아버지
+//    public static int MEMBER_MOTHER = 2;          // 엄마
+//    public static int MEMBER_FATHER = 3;          // 아빠
+//    public static int MEMBER_OLDER_BROTHER = 4;   // 형
+//    public static int MEMBER_OLDER_SISTER = 5;    // 누나
+//    public static int MEMBER_YOUNGER_BROTHER = 6; // 남동생
+//    public static int MEMBER_YOUNGER_SISTER = 7;  // 여동생
+//    public static int MEMBER_ME = 8;              // 나
+
+
+}
