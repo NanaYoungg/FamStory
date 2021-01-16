0950787d63d0985c41c22eedcd176c01a2b97be8
devaspirant0510 <dltmdgh2@sonline20.sen.go.kr>
2021-01-16 15:40:24 +0900

    삭제여부 확인 다이얼로그

---
+++ app/src/main/java/com/hongsam/famstory/dialog/CalendarDeleteAlertDialog.java
@@ -0,0 +1,22 @@
+package com.hongsam.famstory.dialog;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.annotation.NonNull;
+
+public class CalendarDeleteAlertDialog extends AlertDialog.Builder {
+
+
+    public CalendarDeleteAlertDialog(Context context) {
+        super(context);
+    }
+
+    public CalendarDeleteAlertDialog(Context context, int themeResId) {
+        super(context, themeResId);
+    }
+
+}
