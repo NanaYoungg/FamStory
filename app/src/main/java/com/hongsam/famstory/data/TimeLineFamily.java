d6c8f7ebbfed666c7c580f692ddda3266e3b1d99
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-01-20 04:01:02 +0900

    리사이클러뷰 데이터 추가 (타임라인에서 가족정보)

---
+++ app/src/main/java/com/hongsam/famstory/data/TimeLineFamily.java
@@ -0,0 +1,54 @@
+package com.hongsam.famstory.data;
+
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+
+public class TimeLineFamily {
+    Bitmap profile;
+    String name;
+    String nickName;
+    String showMessage;
+    String time;
+
+    public Bitmap getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Bitmap profile) {
+        this.profile = profile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getShowMessage() {
+        return showMessage;
+    }
+
+    public void setShowMessage(String showMessage) {
+        this.showMessage = showMessage;
+    }
+
+    public TimeLineFamily(Bitmap profile, String name, String nickName, String showMessage,String time) {
+        this.profile = profile;
+        this.name = name;
+        this.nickName = nickName;
+        this.showMessage = showMessage;
+        this.time = time;
+    }
+
+
+}
