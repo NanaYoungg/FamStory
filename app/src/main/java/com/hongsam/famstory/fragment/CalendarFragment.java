db1b92d040ac4ce7d41c6465c41ec3b868505749
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-01-22 01:10:35 +0900

    캘린더 프레그먼트 월별일정보기 버튼 추가

--- app/src/main/java/com/hongsam/famstory/fragment/CalendarFragment.java
+++ app/src/main/java/com/hongsam/famstory/fragment/CalendarFragment.java
@@ -3,11 +3,15 @@ package com.hongsam.famstory.fragment;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
 import android.widget.Spinner;
@@ -19,19 +23,34 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.hongsam.famstory.R;
 import com.hongsam.famstory.activitie.MainActivity;
 import com.hongsam.famstory.data.Calendar;
+import com.hongsam.famstory.data.Emotion;
+import com.hongsam.famstory.data.Member;
+import com.hongsam.famstory.database.DBFamstory;
+import com.hongsam.famstory.database.MyMessageDB;
 import com.hongsam.famstory.databinding.FragmentCalendarBinding;
 import com.hongsam.famstory.define.Define;
 import com.hongsam.famstory.dialog.CalendarCustomDialog;
-import com.hongsam.famstory.firebase.CalendarFirebaseDB;
+import com.hongsam.famstory.dialog.MonthCalendarDialog;
+import com.hongsam.famstory.firebase.SpinnerDB;
 import com.hongsam.famstory.firebase.CheckDB;
 import com.hongsam.famstory.firebase.DeleteDB;
 import com.hongsam.famstory.firebase.ReadDB;
 import com.hongsam.famstory.firebase.UpdateDB;
 import com.hongsam.famstory.interf.CallbackInterface;
+import com.hongsam.famstory.service.MyFirebaseMessagingService;
+import com.hongsam.famstory.util.FirebaseManager;
 
+import com.hongsam.famstory.database.MyMessageDB.*;
 
 /**
  * 메인화면에서 일정관리 화면
@@ -39,6 +58,7 @@ import com.hongsam.famstory.interf.CallbackInterface;
  *
  */
 public class CalendarFragment extends Fragment implements CallbackInterface {
+    private SQLiteDatabase sqLiteDatabase;
     private FragmentCalendarBinding mb;
     private MainActivity mainActivity;
     protected View mContentView;
@@ -54,7 +74,6 @@ public class CalendarFragment extends Fragment implements CallbackInterface {
     private int getCalendarYear = nowDate.get(java.util.Calendar.YEAR);
     private int getCalendarMonth = nowDate.get(java.util.Calendar.MONTH)+1;
     private int getCalendarDay = nowDate.get(java.util.Calendar.DATE);
-    CalendarFirebaseDB a123 = new CalendarFirebaseDB();
 
 
 
@@ -84,6 +103,12 @@ public class CalendarFragment extends Fragment implements CallbackInterface {
         root = mb.getRoot();
         mContentView = inflater.inflate(R.layout.fragment_calendar, container, false);
 
+/*        sqLiteDatabase = mainActivity.openOrCreateDatabase("myMsg.db",Context.MODE_PRIVATE,null);
+        String sql = "create table if not exists myMsgTBL"+
+                "(_id integer PRIMARY KEY autoincrement," +
+                "MSG varchar(100),"+
+                "TIME varchar(20))";
+        sqLiteDatabase.execSQL(sql);*/
         if(getFragmentManager()!=null) {
             ft_view_more = getFragmentManager().beginTransaction();
         }
@@ -109,6 +134,8 @@ public class CalendarFragment extends Fragment implements CallbackInterface {
     @Override
     public void onResume() {
         super.onResume();
+        mb.calendarView.setLis
+        // 달력 선택했을때
         mb.calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
@@ -121,7 +148,8 @@ public class CalendarFragment extends Fragment implements CallbackInterface {
                 getCalendarMonth = month;
                 getCalendarDay = dayOfMonth;
 
-                dateToInt = year + month + dayOfMonth;
+                dateToInt = getCalendarMonth  + getCalendarMonth + getCalendarDay;
+                Toast.makeText(getContext(),year+month+dayOfMonth+",",Toast.LENGTH_SHORT).show();
                 ReadDB readDB = new ReadDB(mainActivity);
                 checkDB.checkDB(getCalendarYear,getCalendarMonth,getCalendarDay,getContext(),getFragmentManager(),new CalendarFragment(),
                                 mb.calendarCreate,mb.calendarDelete,mb.calendarUpdate,mb.calendarCreateText,mb.calendarDeleteText,mb.calendarUpdateText);
@@ -145,16 +173,19 @@ public class CalendarFragment extends Fragment implements CallbackInterface {
 
             }
         });
-
+        // 일정생성버튼 눌렀을때
         mb.calendarCreate.setOnClickListener(new View.OnClickListener() {
             final int todayYear = nowDate.get(java.util.Calendar.YEAR);
             final int todayMonth = nowDate.get(java.util.Calendar.MONTH)+1;
             final int todayDay = nowDate.get(java.util.Calendar.DATE);
+            //오늘 날짜랑 캘린더에서 선택된 날짜랑 비교
             @Override
             public void onClick(View v) {
-                int todayDate = todayYear + todayMonth + todayDay;
+                final int todayDate = todayYear + todayMonth + todayDay;
+                int CalendarDate = getCalendarDay+getCalendarYear+getCalendarMonth;
+                Log.e("aaaa","Today"+todayDate+"Calendar"+getCalendarYear);
 
-                if (todayDate>dateToInt) {
+                if (todayDate>CalendarDate) {
                     Toast.makeText(getContext(), "이미 시간이 지난 일정은 생성이 불가능 합니다.", Toast.LENGTH_SHORT).show();
                 } else {
                     CalendarCustomDialog dialog = new CalendarCustomDialog(getCalendarYear, getCalendarMonth, getCalendarDay,mainActivity, Define.CREATE_DIALOG);
@@ -162,11 +193,12 @@ public class CalendarFragment extends Fragment implements CallbackInterface {
                 }
             }
         });
-
+        //삭제버튼 눌렀을때
         mb.calendarDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder alertDialog = new AlertDialog.Builder(getContext());
+                Toast.makeText(getContext(),dateToInt+"",Toast.LENGTH_SHORT).show();
                 alertDialog.setMessage("다시 한번 생각해보세요.\n삭제할경우 복구가 불가능합니다.")
                         .setTitle("후회 하지 않으시겠습니까?");
                 alertDialog.setPositiveButton("네", new DialogInterface.OnClickListener() {
@@ -197,6 +229,7 @@ public class CalendarFragment extends Fragment implements CallbackInterface {
 
             }
         });
+        // 수정버튼 눌렀을때
         mb.calendarUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -212,21 +245,27 @@ public class CalendarFragment extends Fragment implements CallbackInterface {
 
 
 
-
+        // 월별 일정보기 버튼 눌렀을때
         mb.monthCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mainActivity.changeFragment(Define.FRAGMENT_ID_MONTH_LIST);
+                Log.e("dd",getCalendarYear+" "+getCalendarMonth);
+                MonthCalendarDialog monthDialog = new MonthCalendarDialog(mainActivity,getCalendarYear,getCalendarMonth);
+
+                monthDialog.show();
+/*                FragmentManager fm = getFragmentManager();
+                if(fm!=null) {
+                    FragmentTransaction ft = fm.beginTransaction();
+                    MonthCalendarFragment mcf = new MonthCalendarFragment();
+                    ft.add(R.id.calendar_fragment_layout,mcf).commit();
+
+                }*/
+                //mainActivity.changeFragment(Define.FRAGMENT_ID_MONTH_LIST);
 
             }
         });
 
-        mb.spinnerMangerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mainActivity.changeFragment(Define.FRAGMENT_ID_SPINNER_MANGER);
-            }
-        });
+
 
 
     }
@@ -243,31 +282,6 @@ public class CalendarFragment extends Fragment implements CallbackInterface {
     public void setImageResource() {
         // 예시) button1.setBackgroundResource(R.drawable.image1);
     }
-    @Override
-    public void visibleView(int dataIsNull) {
-        Log.e("d",dataIsNull+"");
-
-        if (dataIsNull==Define.DATA_IS_NULL){
-            mb.calendarDeleteText.setVisibility(View.INVISIBLE);
-            mb.calendarUpdateText.setVisibility(View.INVISIBLE);
-            mb.calendarCreateText.setVisibility(View.VISIBLE);
-            mb.calendarUpdate.setVisibility(View.INVISIBLE);
-            mb.calendarDelete.setVisibility(View.INVISIBLE);
-            mb.calendarCreate.setVisibility(View.VISIBLE);
-        }
-        else if (dataIsNull==Define.DATA_IS_NOT_NULL){
-            mb.calendarDeleteText.setVisibility(View.VISIBLE);
-            mb.calendarUpdateText.setVisibility(View.VISIBLE);
-            mb.calendarCreateText.setVisibility(View.INVISIBLE);
-            mb.calendarUpdate.setVisibility(View.VISIBLE);
-            mb.calendarDelete.setVisibility(View.VISIBLE);
-            mb.calendarCreate.setVisibility(View.INVISIBLE);
-
-        }
-        else{
-            Log.e("e","rr0");
-        }
-    }
 
     @Override
     public void view_more_text(Calendar data) {
@@ -276,11 +290,18 @@ public class CalendarFragment extends Fragment implements CallbackInterface {
         String text = data.getDescription();
         String startTime = data.getStartTime();
         String endTime = data.getEndTime();
+        String type = data.getType();
 
 
         mb.vmName.setText("일정명 : " + name);
         mb.vmText.setText("세부내용 : " + text);
         mb.vmTime.setText("시작시간 : " + startTime + "\n" + "종료시간 : " + endTime);
+        mb.type.setText(type);
+    }
+
+    @Override
+    public void visibleView(int dataIsNull) {
+
     }
 
 
