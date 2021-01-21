a3c57be4365473e1d93e47357f39b83bb0f25de0
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-01-22 01:09:29 +0900

    월별 일정보기 프레그먼트

--- app/src/main/java/com/hongsam/famstory/fragment/MonthCalendarFragment.java
+++ app/src/main/java/com/hongsam/famstory/fragment/MonthCalendarFragment.java
@@ -1,40 +1,44 @@
 package com.hongsam.famstory.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.hongsam.famstory.R;
+import com.google.firebase.database.ValueEventListener;
 import com.hongsam.famstory.activitie.MainActivity;
 
-import com.hongsam.famstory.databinding.CalendarViewMoreBinding;
-
-import com.hongsam.famstory.databinding.CalendarViewMoreLayoutBinding;
-
+import com.hongsam.famstory.adapter.MonthAdapter;
+import com.hongsam.famstory.data.Calendar;
+import com.hongsam.famstory.data.MonthCalendar;
 import com.hongsam.famstory.databinding.MonthCalendarLayoutBinding;
+import com.hongsam.famstory.define.Define;
+
+import java.util.ArrayList;
 
 /**
  * CalendarFragment 에서 월별 일정보기
  * devaspirant0510
  * 2021-01-15
  */
-public class MonthCalendar extends Fragment {
+public class MonthCalendarFragment extends Fragment {
 
     MainActivity mainActivity;
     View mContentView;
 
     FirebaseDatabase mDb;
     DatabaseReference mFamRef;
-    private MonthCalendarLayoutBinding mBinding;
+    private MonthCalendarLayoutBinding mb;
     private View root;
 
 
@@ -42,7 +46,7 @@ public class MonthCalendar extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setHasOptionsMenu(true);
-
+        Log.e("d","D");
         mDb = FirebaseDatabase.getInstance();
         mFamRef = mDb.getReference("Family");
 
@@ -59,15 +63,43 @@ public class MonthCalendar extends Fragment {
             return null;
 
         mainActivity = (MainActivity) getActivity();
-        mBinding = MonthCalendarLayoutBinding.inflate(getLayoutInflater());
-        root = mBinding.getRoot();
-        mContentView = inflater.inflate(R.layout.month_calendar_layout, null);
+        mb = MonthCalendarLayoutBinding.inflate(getLayoutInflater());
+        root = mb.getRoot();
+
+        MonthAdapter adapter = new MonthAdapter(getContext());
+        getCalendarDB(adapter,2021,1);
+        mb.monthCalendarList.setAdapter(adapter);
 
 
         return root;
     }
 
+    public void getCalendarDB(final MonthAdapter adapter, int Year, final int Month){
+        FirebaseDatabase fireDB = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = fireDB.getReference(Define.DB_REFERENCE).child(Define.USER).child(Define.CALENDAR_DB).child(Year+"년").child(Month+"월");
+        final ArrayList<String> monthList = new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot val:snapshot.getChildren()){
+                    String day = val.getKey();
+                    Calendar calendar = val.getValue(Calendar.class);
 
+                    adapter.list.add(new MonthCalendar(day,"일정명 : "+calendar.getTitle(),"세부내용 : "+calendar.getDescription()));
+                    adapter.notifyDataSetChanged();
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+    }
     /**
      * 컨트롤 초기화 해주는 함수
      */
